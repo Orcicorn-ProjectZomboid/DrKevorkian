2ad3c8d7ce40b89b0a4c8d4d61e9d30120e5cbe9
Orcicorn <orcicorn@gmail.com>
2022-06-12 00:15:14 -0400

    Fixed Zed spawns for Multiplayer

--- Contents/mods/Kevorkian/media/lua/shared/NPCs/DrDeath_ZombiesZoneDefinition.lua
+++ Contents/mods/Kevorkian/media/lua/shared/NPCs/DrDeath_ZombiesZoneDefinition.lua
@@ -1,34 +1,38 @@
 require 'NPCs/ZombiesZoneDefinition'
+ZombiesZoneDefinition = ZombiesZoneDefinition or {};
 
--- ----------------------------------------------
--- DEBUG ----------------------------------------
--- ----------------------------------------------
---table.insert(ZombiesZoneDefinition.Default,{name = "DrKevorkian", chance=100});
---table.insert(ZombiesZoneDefinition.Default,{name = "DrKevorkianGroupMember", chance=100});
+-- Zone Debugger Spawns (See /lua/shared/NPCs/ZombiesZoneDefinition.lua)
+DrKevorkianGroup = {
+    name = "DrKevorkianGroupMember",
+    chance = 8,
+};
+ZombiesZoneDefinition.Doctor[DrKevorkianGroup] = DrKevorkianGroup;
+ZombiesZoneDefinition.Pharmacist[DrKevorkianGroup] = DrKevorkianGroup;
 
-
--- ----------------------------------------------
--- Dr. Kevorkian Group Members ------------------
--- ----------------------------------------------
--- Zones
-table.insert(ZombiesZoneDefinition.Doctor, {name = "DrKevorkianGroupMember", chance=5});
-table.insert(ZombiesZoneDefinition.NursingHome, {name = "DrKevorkianGroupMember", chance=10});
--- Rooms
-table.insert(ZombiesZoneDefinition.Default, {name = "DrKevorkianGroupMember", chance=5, room="medical"});
-table.insert(ZombiesZoneDefinition.Default, {name = "DrKevorkianGroupMember", chance=5, room="medclinic"});
-table.insert(ZombiesZoneDefinition.Default, {name = "DrKevorkianGroupMember", chance=2, room="hospitalroom"});
-table.insert(ZombiesZoneDefinition.Default, {name = "DrKevorkianGroupMember", chance=2, room="medicalstorage"});
--- Random Mobs
-table.insert(ZombiesZoneDefinition.Default, {name = "DrKevorkianGroupMember", chance=1});
+DrKevorkian = {
+    name = "DrKevorkian",
+    gender = "male",
+    chance = 1,
+};
+ZombiesZoneDefinition.Doctor[DrKevorkian] = DrKevorkian;
 
 
--- ----------------------------------------------
--- Dr. Kevorkian --------------------------------
--- ----------------------------------------------
--- Zones
-table.insert(ZombiesZoneDefinition.Doctor, {name = "DrKevorkian", chance=1});
-table.insert(ZombiesZoneDefinition.NursingHome, {name = "DrKevorkian", chance=10});
--- Rooms
-table.insert(ZombiesZoneDefinition.Default, {name = "DrKevorkian", chance=1, room="medical"});
-table.insert(ZombiesZoneDefinition.Default, {name = "DrKevorkian", chance=1, room="medclinic"});
-table.insert(ZombiesZoneDefinition.Default, {name = "DrKevorkian", chance=5, room="hospitalroom"});
+-- General Population Spawns
+table.insert(ZombiesZoneDefinition.Default, {name="DrKevorkianGroupMember", chance=5});
+table.insert(ZombiesZoneDefinition.Default, {name="DrKevorkian", chance=0.1, gender="male"});
+
+-- Kevorkian Group Doctors
+table.insert(ZombiesZoneDefinition.Default, {name="DrKevorkianGroupMember", chance=10, room="medical"});
+table.insert(ZombiesZoneDefinition.Default, {name="DrKevorkianGroupMember", chance=10, room="medclinic"});
+table.insert(ZombiesZoneDefinition.Default, {name="DrKevorkianGroupMember", chance=10, room="hospitalstorage"});
+table.insert(ZombiesZoneDefinition.Default, {name="DrKevorkianGroupMember", chance=10, room="hospitalroom"});
+table.insert(ZombiesZoneDefinition.Default, {name="DrKevorkianGroupMember", chance=10, room="medicalstorage"});
+table.insert(ZombiesZoneDefinition.Default, {name="DrKevorkianGroupMember", chance=10, room="pharmacy"});
+table.insert(ZombiesZoneDefinition.Default, {name="DrKevorkianGroupMember", chance=20, room="pharmacystorage"});
+
+-- Dr Kevorkian
+table.insert(ZombiesZoneDefinition.Default, {name="DrKevorkian", chance=5, room="medical", gender="male"});
+table.insert(ZombiesZoneDefinition.Default, {name="DrKevorkian", chance=5, room="medclinic", gender="male"});
+table.insert(ZombiesZoneDefinition.Default, {name="DrKevorkian", chance=2, room="hospitalroom", gender="male"});
+table.insert(ZombiesZoneDefinition.Default, {name="DrKevorkian", chance=10, room="medicaloffice", gender="male"});
+table.insert(ZombiesZoneDefinition.Default, {name="DrKevorkian", chance=1, room="pharmacystorage", gender="male"});
